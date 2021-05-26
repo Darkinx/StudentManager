6e666db6620535f353c4e3c80043eb3435db4c6f
Darkinx <razonadojohn@gmail.com>
2021-05-26 09:39:14 +0800

    Changes height of the main window

--- Student_Management_System/src/com/StudentManagement/StudentManagerUI.java
+++ Student_Management_System/src/com/StudentManagement/StudentManagerUI.java
@@ -32,7 +32,7 @@ public class StudentManagerUI extends JFrame {
 	 */
 	public StudentManagerUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 758, 499);
+		setBounds(100, 100, 758, 1000);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
