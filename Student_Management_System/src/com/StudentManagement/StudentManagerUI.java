522fd73c5564981d100a3b1ee247f8af2d7b90e4
Darkinx <razonadojohn@gmail.com>
2021-05-28 22:04:42 +0800

    Added title to main Windows
    Added title to the name window and called it SNSM for the mean time, not
    yet final on that one thou.

--- Student_Management_System/src/com/StudentManagement/StudentManagerUI.java
+++ Student_Management_System/src/com/StudentManagement/StudentManagerUI.java
@@ -3,6 +3,7 @@ package com.StudentManagement;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Toolkit;
 
 public class StudentManagerUI extends JFrame {
 
@@ -26,6 +27,8 @@ public class StudentManagerUI extends JFrame {
 	 * Create the frame.
 	 */
 	public StudentManagerUI() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\[A] Documents\\[1] WORKING files\\IN PROGRESS [This Week]\\Piling Larang Manwal\\Pictures\\Images\\SNSM logo_Text.png"));
+		setTitle("SNSM-Welcome");
 		setBounds(100, 100, 856, 477);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
