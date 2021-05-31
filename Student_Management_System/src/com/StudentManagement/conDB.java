fae2edae56f343768c3745cd061fe1380fbbc2fc
Darkinx <razonadojohn@gmail.com>
2021-05-31 23:48:04 +0800

    Added comments and borders
    Added comments to conDB
    Having and fixing the Layout of the MainUI since Windows Builder suck
    ass.

--- Student_Management_System/src/com/StudentManagement/conDB.java
+++ Student_Management_System/src/com/StudentManagement/conDB.java
@@ -11,6 +11,11 @@ import java.util.Locale;
  * @author Darkinx
  *
  */
+
+/** Need to address the need of independent initiation of the database
+ * and its security capability
+ * this is temporary for now too prioritize the main functionality.
+*/
 public class conDB {
 	public Connection connectionString() {
 		Connection conn = null;
