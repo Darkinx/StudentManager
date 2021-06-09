6cea87b566f392c83fd0e8b8ad2cb4da0742c295
Darkinx <razonadojohn@gmail.com>
2021-06-10 00:40:50 +0800

    Change StudentManagerUI to MainPage
    Added some GUI for the MainPage
    This is still rough, need to have an autoresizing method

---
+++ Student_Management_System/src/com/StudentManagement/MainPageFunction.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.StudentManagement;
+
+import java.awt.Image;
+
+/**
+ * @author Darkin
+ *
+ */
+public class MainPageFunction {
+//
+//	Image img = icon.getImage() ;
+//	Image newimg = img.getScaledInstance( NEW_WIDTH, NEW_HEIGHT,  java.awt.Image.SCALE_SMOOTH ) ;  
+//	icon = new ImageIcon( newimg );
+}
